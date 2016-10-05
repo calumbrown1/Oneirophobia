ac5122aee5dbd048d4b39c2c7bad9d02c1a5da23
calumbrown1 <calumbrown1@Hotmail.com>
2016-10-05 19:07:43 +0100

    Added Project Files
    -Added Unity Project
    -Added Menu Scene with Navigation Buttons
    -Added 5 Blank scenes for nightmares

---
+++ Oneirophobia/Assets/Scripts/MenuScripts/MenuControllerScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuControllerScript : MonoBehaviour {
+
+    public void LoadLevel1()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void LoadLevel2()
+    {
+        SceneManager.LoadScene("Level2");
+    }
+}
