4080a2594f33a3efc9b40b3d5c317e1a8285d18b
Euan <euanmcmen@hotmail.com>
2016-10-16 21:12:22 +0100

    More work on the giants
    Added materials for the giants.
    
    Also created the player script.

---
+++ Oneirophobia/Assets/Scripts/MegalophobiaScripts/GaintMovementScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GaintMovementScript : MonoBehaviour
+{
+    //Giant walking speed.
+    public float walkingSpeed;
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //Move forward.
+        transform.position += transform.forward * Time.deltaTime * walkingSpeed;
+	}
+}
