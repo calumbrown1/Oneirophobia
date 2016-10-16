4671f5151d889f1943ee600a16334ac8b217a033
Euan <euanmcmen@hotmail.com>
2016-10-16 22:02:35 +0100

    Added playerscript.

--- Oneirophobia/Assets/Scripts/MegalophobiaScripts/PlayerScript.cs
+++ Oneirophobia/Assets/Scripts/MegalophobiaScripts/PlayerScript.cs
@@ -1,15 +1,39 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerScript : MonoBehaviour {
-
+public class PlayerScript : MonoBehaviour
+{
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 	
 	}
+
+    /// <summary>
+    /// Trigger collision handler.
+    /// You tend to wake up before a death event in a dream, so the death event should be triggered just before.  Large trigger zones can simulate this.
+    /// </summary>
+    /// <param name="col">The colliding trigger's collider component.</param>
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "DeathTrigger")
+        {
+            Death();
+        }
+    }
+
+    /// <summary>
+    /// This kills the player.
+    /// When one of the defeat conditions is met, this method is triggered.
+    /// </summary>
+    public void Death()
+    {
+        print("Player has died.");
+    }
 }
